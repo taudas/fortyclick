89654321e8bcf771b318d024d9cf8528e91e3d59
tony audas <taudas@gmail.com>
2019-07-15 22:48:15 -0400

    add relax and launcher.png

--- lib/main.dart
+++ lib/main.dart
@@ -17,16 +17,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Forty Click',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.green,
+              primarySwatch: Colors.green,
       ),
       home: MyHomePage(title: 'Forty Click - Some Say This is Relaxing'),
     );
@@ -35,16 +26,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -150,23 +131,27 @@ class _MyHomePageState extends State<MyHomePage> {
 //      ), // This trailing comma makes auto-formatting nicer for build methods.
     return DefaultTabController(
       length: 1,
+      child: Container(
+          decoration: BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage("relax.png"), fit: BoxFit.cover)),
       child: Scaffold(
-        appBar: AppBar(
-          bottom: TabBar(
-            tabs: [
-              Tab(text: 'Some Say this Sound is Relaxing'),
-            ],
+          appBar: AppBar(
+            bottom: TabBar(
+              tabs: [
+                Tab(text: 'Some Say this Sound is Relaxing'),
+              ],
+            ),
+            title: Text('Forty Click'),
           ),
-          title: Text('Forty Click'),
-        ),
-        body: ListView(
-          children: [localAsset(),
-            AdmobBanner(
-            adUnitId: getBannerAdUnitId(),
-            adSize: bannerSize,
-    )],
+          body: ListView(
+            children: [localAsset(),
+              AdmobBanner(
+              adUnitId: getBannerAdUnitId(),
+              adSize: bannerSize,
+      )],
 
-    )));
+      ))));
   }
 }
 String getAppId() {
